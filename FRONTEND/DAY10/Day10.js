8f58994bb85ee00f58d28e802585c281a0b0c5d9
Deepanshu Singhal <gunni.deepanshu@gmail.com>
2026-05-22 16:34:31 +0530

    Added DAY10 files

---
+++ FRONTEND/DAY10/Day10.js
@@ -0,0 +1,40 @@
+console.log("Hello JavaScript");
+
+console.log("My name is Deepanshu and I live in Delhi NCR and my age is 22")
+
+console.warn("This is a warning message")
+
+console.error("This is an error message")
+
+console.table({number1  : 1, number2 : 2, number3 : 3, number4 : 4, number5 : 5})
+
+var StudentName = "Deepanshu"
+
+var age = 22
+console.log(age)
+
+var a = 10
+var b = 20
+a = 20
+b = 10
+console.log(a,b)
+
+var c
+console.log(c)
+
+var score = 20
+console.log("Your total score is ", 49+score)
+
+var FirstName = "Deepanshu"
+var LastName = "Singhal"
+var d = null
+
+console.log(typeof(FirstName))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+
+var e = 10000000000000000000000000n
+console.log(e)
+
+
